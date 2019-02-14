aa578bf2c503401a12f30c6411a71824abca86e8
bais117 <1050062540@qq.com>
2019-02-14 16:00:00 +0800

    登陆路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '../components/login.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -8,6 +8,10 @@ export default new Router({
     {
       path: '/',
       name: 'home'
+    }, {
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
