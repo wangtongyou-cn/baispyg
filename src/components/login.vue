2a0f287b4fa389acae58b12f5fb24bdb1089e4f8
bais117 <1050062540@qq.com>
2019-02-14 16:20:47 +0800

    引入登录组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>login---</div>
+  <div>
+     
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+         <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+        <el-button class="login-button" type="success">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+        formdata:{
+            username:'',
+            password:''
+        }
+    };
+  }
+};
 </script>
-
+    
 <style>
-
 </style>
