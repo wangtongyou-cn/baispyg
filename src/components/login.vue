8a0d429041872a3a17f8d1d6d13f5a8574670e39
bais117 <1050062540@qq.com>
2019-02-15 10:20:37 +0800

    token值

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="login-wrap">
-     
     <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-         <h2>用户登录</h2>
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-        <el-button 
-        @click.prevent="handleLogin()" class="login-button" type="success">登录</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-button" type="success">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -19,30 +17,36 @@
 export default {
   data() {
     return {
-        formdata:{
-            username:'',
-            password:''
-        }
+      formdata: {
+        username: "",
+        password: ""
+      }
     };
   },
   methods: {
-    async handleLogin(){
+    async handleLogin() {
       //ES7新特性 async await
-      
+
       //前提api-server->node app启动服务器
-      const res = await this.$http.post(`login`,this.formdata)
-      console.log(res)
-      const {data:{data,meta:{msg,status}}} = res
-      if(status === 200) {
-          // console.log('success--')
-          this.$router.push({
-            name:"home"
-          })
-        } else {
-          // console.log('err---')
-           this.$message.error(msg)
+      const res = await this.$http.post(`login`, this.formdata);
+      console.log(res);
+      const {
+        data: {
+          data: { token },
+          meta: { msg, status }
         }
-        
+      } = res;
+      if (status === 200) {
+        localStorage.setItem("token",token)
+        // console.log('success--')
+        this.$router.push({
+          name: "home"
+        });
+      } else {
+        // console.log('err---')
+        this.$message.error(msg);
+      }
+
       // .then((res)=> {
       //   console.log(res)
       //   //多层对象解构赋值
