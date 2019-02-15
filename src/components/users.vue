9e0e818b9e886c1daa56519ac529b5a38e8d1ed6
bais117 <1050062540@qq.com>
2019-02-15 22:03:36 +0800

    没写完的

--- src/components/users.vue
+++ src/components/users.vue
@@ -11,22 +11,46 @@
     <!-- 搜素 -->
     <el-row class="search">
       <el-rol>
-        <el-input placeholder="请输入内容" v-model="query"  class="searchInput">
+        <el-input placeholder="请输入内容" v-model="query" class="searchInput">
           <el-button slot="append" icon="el-icon-search"></el-button>
         </el-input>
-         <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain>添加用户</el-button>
       </el-rol>
     </el-row>
+    <!-- 表格部分 -->
+    <!-- el-table 表格 data 绑定数据[]
+    el-table-column 控制的是列 -->
+    <el-table :data="list" style="width: 100%">
+      <el-table-column prop="id" label="#" width="80"></el-table-column>
+      <el-table-column prop="usename" label="姓名" width="120"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+      <el-table-column prop="create_time" label="创建日期" width="140"></el-table-column>
+      <el-table-column prop="address" label="用户状态" width="140"></el-table-column>
+      <el-table-column prop="address" label="操作" width="200"></el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            query: ''
-        }
+  data() {
+    return {
+      query: "",
+      pagenum: "",
+      pagesize: "",
+    //   表格数据
+      list: []
+    };
+  },
+  created() {
+    this.getTableDate();
+  },
+  methods: {
+    async getTableDate() {
+      const res =await this.$http.get(`users?query=this.${query}&pagenum=this.${pagenum}&pagesize=this.${pagesize}`)
     }
+  }
 };
 </script>
 
@@ -34,10 +58,10 @@ export default {
 .box {
   height: 100%;
 }
-.search{
-    margin-top: 20px;
+.search {
+  margin-top: 20px;
 }
-.searchInput{
-    width: 350px;
+.searchInput {
+  width: 350px;
 }
 </style>
