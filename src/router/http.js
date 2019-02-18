ecb6d5da5ca83552e69ab6cba9373357900b20db
bais117 <1050062540@qq.com>
2019-02-18 12:00:49 +0800

    111

--- src/router/http.js
+++ src/router/http.js
@@ -4,7 +4,10 @@ import axios from 'axios'
 const HttpServer = {}
 
 HttpServer.install = function (Vue, options) {
+
   axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
+  // 拦截
   const AUTH_TOKEN = localStorage.getItem('token')
   axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
   Vue.prototype.$http = axios
