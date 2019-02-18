f973d87c55338706b0106b57e2033fcec87fe6b1
bais117 <1050062540@qq.com>
2019-02-18 20:03:04 +0800

    拦截器

--- src/router/http.js
+++ src/router/http.js
@@ -4,12 +4,20 @@ import axios from 'axios'
 const HttpServer = {}
 
 HttpServer.install = function (Vue, options) {
-
   axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
-  // 拦截
+  // 所有请求发起后,需要筛选
+  axios.interceptors.request.use(function (config) {
+    console.log('请求被拦截了')
+    if (config.url !== 'login') {
+      // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
+      config.headers['Authorization'] = AUTH_TOKEN
+    }
+    return config
+  }, function (error) {
+    return Promise.reject(error)
+  })
   const AUTH_TOKEN = localStorage.getItem('token')
-  axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
   Vue.prototype.$http = axios
 }
 
