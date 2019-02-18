c4e147045f5c3b908d2e6fd9dc0aa81342581355
bais117 <1050062540@qq.com>
2019-02-18 21:17:37 +0800

    第三次重置

--- src/components/home.vue
+++ src/components/home.vue
@@ -27,40 +27,6 @@
               <i class="el-icon-location"></i>
               <span>{{item1.authName}}</span>
             </template>
-            <el-menu-item-group>
-              <el-menu-item index="roles"><i class="el-icon-menu"></i>角色列表</el-menu-item>
-              <el-menu-item index="rights"><i class="el-icon-menu"></i>权限列表</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1"><i class="el-icon-menu"></i>商品列表</el-menu-item>
-               <el-menu-item index="1-1"><i class="el-icon-menu"></i>分类参数</el-menu-item>
-                <el-menu-item index="1-1"><i class="el-icon-menu"></i>商品分类</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1"><i class="el-icon-menu"></i>订单列表</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1"><i class="el-icon-menu"></i>数据报表</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
 
             <el-menu-item
               :index="item2.path+''"
