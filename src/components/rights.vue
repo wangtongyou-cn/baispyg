58fe4d6bd20f9a1b22be0225737e0ddb96d8a149
bais117 <1050062540@qq.com>
2019-02-18 20:14:33 +0800

    权限列表完成

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -3,31 +3,47 @@
     <!-- 面包屑 -->
     <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
     <!-- 表格 -->
+    <el-table :data="list" style="width: 100%" height="350px">
+      <el-table-column type="index" label="#" width="160"></el-table-column>
+      <el-table-column prop="authName" label="权限名称" width="200"></el-table-column>
+      <el-table-column prop="path" label="路径" width="200"></el-table-column>
+      <el-table-column prop="pid" label="层级" width="200">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level ==='0'">一级</span>
+          <span v-if="scope.row.level ==='1'">二级</span>
+          <span v-if="scope.row.level ==='2'">三级</span>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-  created () {
-    this.getTableData()
+  data() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    this.getTableData();
   },
   methods: {
-    async getTableData () {
-    //   const AUTH_TOKEN = localStorage.getItem("token");
-    //   this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
-
-      const res = await this.$http.get(`rights/list`)
-      console.log(res)
+    async getTableData() {
+      //   const AUTH_TOKEN = localStorage.getItem("token");
+      //   this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+      const res = await this.$http.get(`rights/list`);
+      console.log(res);
       const {
         data,
         meta: { msg, status }
-      } = res.data
+      } = res.data;
       if (status === 200) {
-        this.list = data
+        this.list = data;
       }
     }
   }
-}
+};
 </script>
 
 <style>
