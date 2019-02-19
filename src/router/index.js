61c5a110eb8a5da4799b8892733b38a5c20326ea
bais117 <1050062540@qq.com>
2019-02-19 20:52:35 +0800

    又出错了,git

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import {
+  Message
+} from 'element-ui'
 import Login from '../components/login.vue'
 import Home from '../components/home.vue'
 import Users from '@/components/users.vue'
@@ -7,13 +10,12 @@ import Rights from '@/components/rights.vue'
 import Roles from '@/components/roles.vue'
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      children: [{
+const router = new Router({
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home,
+    children: [{
         path: 'users',
         name: '/users',
         component: Users
@@ -27,11 +29,30 @@ export default new Router({
         path: 'roles',
         name: '/roles',
         component: Roles
-      }]
-    }, {
-      name: 'login',
-      path: '/login',
-      component: Login
-    }
-  ]
+      }
+    ]
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  if (to.name === 'login') {
+    next()
+  } else {
+    const token = localStorage.getItem("token")
+    if(!token) {
+      Message.warning("请先登录")
+      router.push({
+        name: 'login'
+      })
+      return;
+    }
+    next()
+  }
+})
+
+export default router
