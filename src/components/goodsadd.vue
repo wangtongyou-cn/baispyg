7eea89e0ac610c6f49aeaff6a0e21e1d29d4e8a2
bais117 <1050062540@qq.com>
2019-02-22 10:40:38 +0800

    富文本引入

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -48,16 +48,51 @@
           </el-form-item>
         </el-tab-pane>
 
-        <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="3" label="商品属性">
+          <el-form-item :label="item.attr_name" v-for="(item,i) in arrStaic" :key="item.attr_id">
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
+        <!-- 上传图片 -->
+        <el-tab-pane name="4" label="商品图片">
+          <el-upload
+            action="http://localhost:8888/api/private/v1/upload"
+            :headers="headers"
+            :on-remove="handleRemove"
+            :on-success="handleSuccess"
+            list-type="picture"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+          </el-upload>
+        </el-tab-pane>
 
-        <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">
+          <el-form-item>
+            <el-button @click="addgoods()">添加商品</el-button>
+            <!-- 富文本 -->
+            <quill-editor
+            v-model="form.goods_intorduce"
+            
+            ></quill-editor>
+          </el-form-item>
+          
+        </el-tab-pane>
       </el-tabs>
     </el-form>
   </el-card>
 </template>
 <script>
+// 引入富文本
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+ 
+import { quillEditor } from 'vue-quill-editor'
+
 export default {
+  components: {
+    quillEditor
+  },
   data() {
     return {
       active: "1",
@@ -80,19 +115,33 @@ export default {
       },
       arrDy: [],
       checkList: [],
-      arrStaic: []
+      arrStaic: [],
+      headers: {
+        Authorization:localStorage.getItem('token')
+      }
     };
   },
   created() {
     this.getGoodsCate();
   },
   methods: {
+    // 添加商品
+    addgoods(){},
+    //上传图片
+    handleRemove(file,fileList) {
+      console.log(file,fileList);
+      file.response.data.tmp_path;
+    },
+    handleSuccess(response,file,fileList) {
+      console.log(response);
+    },
+
     async changeTab() {
       //   if(this.active==="3") {
 
       //   };
 
-      if (this.active === "2"|| this.active === "3") {
+      if (this.active === "2" || this.active === "3") {
         if (this.selectedOptions.length !== 3) {
           this.$message.error("请先选择三级分类");
           return;
@@ -108,40 +157,38 @@ export default {
           } = res.data;
           if (status === 200) {
             this.arrStaic = data;
-            console.log(this.arrStaic)
-            };
-          }
-        }
-
-        // 动态参数数据
-        if (this.active === "2") {
-          const res = await this.$http.get(
-            `categories/${this.selectedOptions[2]}/attributes?sel=many`
-          );
-          //  console.log(res);
-
-          const {
-            meta: { msg, status },
-            data
-          } = res.data;
-          if (status === 200) {
-            this.arrDy = data;
-            this.arrDy.forEach(item => {
-              //   if (item.attr_vals.leng === 0) {
-              //     item.attr_vals = [];
-              //   } else {
-              //     item.attr_vals = item.attr_vals.trim().split(",");
-              //   }
-              item.attr_vals =
-                item.attr_vals.trim().length === 0
-                  ? []
-                  : item.attr_vals.trim().split(",");
-            });
+            // console.log(this.arrStaic);
           }
         }
       }
-    ,
 
+      // 动态参数数据
+      if (this.active === "2") {
+        const res = await this.$http.get(
+          `categories/${this.selectedOptions[2]}/attributes?sel=many`
+        );
+        //  console.log(res);
+
+        const {
+          meta: { msg, status },
+          data
+        } = res.data;
+        if (status === 200) {
+          this.arrDy = data;
+          this.arrDy.forEach(item => {
+            //   if (item.attr_vals.leng === 0) {
+            //     item.attr_vals = [];
+            //   } else {
+            //     item.attr_vals = item.attr_vals.trim().split(",");
+            //   }
+            item.attr_vals =
+              item.attr_vals.trim().length === 0
+                ? []
+                : item.attr_vals.trim().split(",");
+          });
+        }
+      }
+    },
     async getGoodsCate() {
       const res = await this.$http.get(`categories?type=3`);
 
@@ -154,9 +201,8 @@ export default {
       }
     },
     handleChange() {}
-    }
   }
-;
+};
 </script>
 
 <style>
@@ -178,5 +224,8 @@ export default {
 .form {
   height: 350px;
   overflow: auto;
+}
+.ql-container {
+  min-height: 200px
 }
 </style>
