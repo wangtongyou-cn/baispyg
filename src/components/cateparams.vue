b109224bce5d97aa44326d11127252d6dce0843b
bais117 <1050062540@qq.com>
2019-02-24 10:34:12 +0800

    分类参数完成

--- src/components/cateparams.vue
+++ src/components/cateparams.vue
@@ -57,7 +57,20 @@
         </el-table>
       </el-tab-pane>
       <el-tab-pane label="静态参数" name="2">
-        静态态参数
+        <el-button disable>设置静态参数</el-button>
+        <el-table height="450px" border stripe :data="arrStaic" style="width: 100%">
+          <!-- 序号 -->            
+          <el-table-column type="index" label="#" width="120"></el-table-column>
+
+          <el-table-column prop="attr_name" label="属性名称"></el-table-column>
+<el-table-column prop="attr_vals" label="属性值"></el-table-column>
+          <el-table-column label="操作" width="100">
+            <template slot-scope="scope">
+              <el-button plain size="mini" type="primary" icon="el-icon-edit" circle></el-button>
+              <el-button plain size="mini" type="danger" icon="el-icon-delete" circle></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
       </el-tab-pane>
     </el-tabs>
   </el-card>
